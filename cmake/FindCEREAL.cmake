f71de9489b1619c84b1421fbc95ff6396502d001
Irina Demeshko <irina@lanl.gov>
2016-10-14 11:32:02 -0600

    Added "CEREAL_INCLUDE_DIRS" that is 1 directory up of the "CEREAL_INCLUDE_DIR"

--- cmake/FindCEREAL.cmake
+++ cmake/FindCEREAL.cmake
@@ -12,11 +12,13 @@ find_path(CEREAL_INCLUDE_DIR cereal.hpp
   PATHS ${CEREAL_ROOT}
   PATH_SUFFIXES include/cereal)
 
+set (CEREAL_INCLUDE_DIRS ${CEREAL_INCLUDE_DIR}/../)
+
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LAPACKE_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(CEREAL REQUIRED_VARS CEREAL_INCLUDE_DIR)
+find_package_handle_standard_args(CEREAL REQUIRED_VARS CEREAL_INCLUDE_DIR CEREAL_INCLUDE_DIRS)
 
-mark_as_advanced(CEREAL_INCLUDE_DIR)
+mark_as_advanced(CEREAL_INCLUDE_DIR CEREAL_INCLUDE_DIRS)
 
 
